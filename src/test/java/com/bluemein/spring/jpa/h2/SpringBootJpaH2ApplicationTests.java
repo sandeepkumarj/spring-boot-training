50ba3ff1738620c17de55a63ed119c68fd34e81f
Sandeep Kumar <sandeepkumar@Sandeeps-MacBook-Pro.local>
2023-12-21 01:39:59 +0530

    initial commit

---
+++ src/test/java/com/bluemein/spring/jpa/h2/SpringBootJpaH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluemein.spring.jpa.h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
